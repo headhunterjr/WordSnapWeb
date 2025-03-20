20ae2259d0698d607c955d6fd5f9dd822d4be305
Tyshyk V <tyshykviktoria77@gmail.com>
2025-03-21 00:08:00 +0200

    added UserController.cs

---
+++ WordSnapWeb/WordSnapWeb/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WordSnapWeb.Models;
+
+namespace WordSnapWeb.Controllers
+{
+    [Route("User/{username}")]
+    public class UserController : Controller
+    {
+        private readonly IWordSnapRepository _repository;
+        // test user id and username
+        private const int CurrentUserId = 82;
+        private const string CurrentUsername = "test_username";
+
+        public UserController(IWordSnapRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("SavedLibrary")]
+        public async Task<IActionResult> SavedLibrary(string username)
+        {
+            if (username != CurrentUsername)
+            {
+                return NotFound();
+            }
+
+            var cardsets = await _repository.GetUsersCardsetsLibraryAsync(CurrentUserId);
+            ViewBag.Username = username;
+            return View(cardsets);
+        }
+    }
+}
